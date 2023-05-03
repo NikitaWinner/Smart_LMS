55543e8b959dd0e297d866becd90eb4b06595b38
Nikita <kyle.winner@gmail.com>
2023-05-03 18:16:38 +0300

    Template's context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,20 @@
 from django.views.generic import TemplateView
 
 
-class MainPageView(TemplateView):  # контроллер
-    template_name = "mainapp/index.html"  # Путь к шаблону относительно директории templates
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
 
 
 class CoursesPageView(TemplateView):
